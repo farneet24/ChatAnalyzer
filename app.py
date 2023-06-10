03ba06b4fa29a3d8aa1599c84a632b8e0f9c804c
farneet24 <111075591+farneet24@users.noreply.github.com>
2023-06-11 01:15:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -101,8 +101,8 @@ if uploaded_file is not None:
                 # st.pyplot(fig)
                 
                 chart = alt.Chart(data).mark_bar().encode(
-                    alt.X('User:N'),
-                    alt.Y('Number of messages:Q'),
+                    X = 'User:N',
+                    Y = 'Number of messages:Q',
                     color = alt.Color('User:N', scale=alt.Scale(scheme='inferno'))
                 ).properties(
                     height = 500,
